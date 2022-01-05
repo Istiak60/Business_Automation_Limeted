c51b6f3e8d6b2337302321618c1b35e64e076731
Istiak60 <istisquare@gmail.com>
2022-01-05 16:31:08 +0600

    database connected by pdo

--- send&fetch(session-3)/index.php
+++ send&fetch(session-3)/index.php
@@ -1,3 +1,39 @@
+<?php
+ session_start();
+ $conn = new PDO(
+    "mysql:host=localhost;dbname=business_automation_limited",
+    'root',
+    ''
+);
+//set the PDO error mode to exception
+$conn->setAttribute(
+    PDO::ATTR_ERRMODE,
+    PDO::ERRMODE_EXCEPTION
+);
+    if($_SERVER['REQUEST_METHOD']== "POST")
+    {
+      //somthing was posted
+     $username = $_POST['username'];
+     $password = $_POST['password'];
+     $email = $_POST['email'];
+
+$query = "INSERT INTO `user` (`username`,`password`,`email`) VALUES (:username,:password,:email);";
+$stmt = $conn->prepare($query);
+$stmt->bindParam(':username', $username);
+$stmt->bindParam(':password', $password);
+$stmt->bindParam(':email', $email);
+$result = $stmt->execute();
+
+var_dump($result);
+header("location:index.php");
+    
+     
+    }
+
+
+
+?>
+
 
 
 <!DOCTYPE html>
@@ -10,15 +46,13 @@
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/jquery.steps.css">
 </head>
-
 <body>
-   
     <form id="my-form" method="post">
         <div>
             <h3>Account</h3>
             <section>
                 <label for="userName">User name *</label>
-                <input id="userName" name="userName" type="text" >
+                <input id="userName" name="username" type="text" >
                 <label for="password">Password *</label>
                 <input id="password" name="password" type="password" >
                 <label for="confirm">Confirm Password *</label>
@@ -27,46 +61,46 @@
             </section>
             <h3>Profile</h3>
             <section>
-                <label for="name">First name *</label>
+                <!-- <label for="name">First name *</label>
                 <input id="name" name="name" type="text" >
                 <label for="surname">Last name *</label>
                 <input id="surname" name="surname" type="text" >
                 <label for="dob">Date of Birth *</label>
                 <input id="dob" name="dob" type="date" >
                 <label for="email">Email *</label>
-                <input id="email" name="email" type="text" class=" email">
                 <label for="address">Address</label>
-                <input id="address" name="address" type="text">
+                <input id="address" name="address" type="text"> -->
+                <input id="email" name="email" type="text" >
 
                 <p>(*) Mandatory</p>
             </section>
             <h3>Work and Education</h3>
             <section>
-            <label for="School">School  (If applicable)</label>
+            <!-- <label for="School">School  (If applicable)</label>
                 <input id="school" name="school" type="text" >
                 <label for="college">College    (If applicable)</label>
                 <input id="college" name="college" type="text" >
                 <label for="university">University  (If applicable)</label>
                 <input id="university" name="university" type="text" >
-                
+                 -->
                 
             </section>
             <h3>Social Accounts</h3>
             <section>
-                <label for="Facebook">Facebook *</label>
-                <input id="fbName" name="fbName" type="text" >
+                <!-- <label for="Facebook">Facebook *</label>
+                <input id="fbName" name="fbname" type="text" >
                 <label for="Github">Github *</label>
                 <input id="github" name="github" type="text" >
                 <label for="linkedIn">Linked In *</label>
-                <input id="linkedIn" name="linkedIn" type="text" >
+                <input id="linkedIn" name="linkedin" type="text" >
                 <label for="Whatsapp">Whatsapp</label>
                 <input id="whatsapp" name="whatsapp" type="text" >
-               
+                -->
                 <p>(*) Mandatory</p>
             </section>
             <h3>Finish</h3>
             <section>
-                <input id="acceptTerms" name="acceptTerms" type="checkbox" > <label for="acceptTerms">I agree with the Terms and Conditions.</label>
+                <!-- <input id="acceptTerms" name="acceptTerms" type="checkbox" > <label for="acceptTerms">I agree with the Terms and Conditions.</label> -->
             </section>
         </div>
     </form>
